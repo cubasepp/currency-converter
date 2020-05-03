e662ba11dd99f1a51be139061defe3a09411654c
Michael <info@mv-web.net>
2020-05-03 12:56:36 +0200

    Fixes

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -2,8 +2,8 @@ class ExchangeRatesController < ApplicationController
   def index
     data = ExchangeRate.where(date: 1.week.ago..Time.now).order(date: :asc).all
 
-    @labels = data.map { |r| r.date }.uniq
-    @exchange_rates = data.group_by { |rate| (rate[:from_currency] + rate[:to_currency]) }
-                          .map { |k,v| { label: k, data: v.map { |r| r.rate } } }
+    @labels = data.map { |rates| rates.date }.uniq
+    @exchange_rates = data.group_by { |currency| (currency[:from_currency] + currency[:to_currency]) }
+                          .map { |key, value| { label: key, data: value.map { |r| r.rate } } }
   end
 end
