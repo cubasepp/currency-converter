c76ba1ba40bd1f746ae14b036c3658c0eb9252bc
Michael <info@mv-web.net>
2020-05-03 12:04:21 +0200

    Checkstyle

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -4,6 +4,6 @@ class ExchangeRatesController < ApplicationController
 
     @labels = data.map { |r| r.date }.uniq
     @exchange_rates = data.group_by { |rate| (rate[:from_currency] + rate[:to_currency]) }
-                          .map{ |k,v| { label: k, data: v.map { |r| r.rate }} }
+                          .map { |k,v| { label: k, data: v.map { |r| r.rate } } }
   end
 end
