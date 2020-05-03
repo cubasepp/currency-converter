c6dc2505dfe90adcce5ee961e0105fb98e90e85a
Michael <info@mv-web.net>
2020-05-03 12:48:36 +0200

    Improvements

--- app/views/convert/index.json.jbuilder
+++ app/views/convert/index.json.jbuilder
@@ -1,3 +1,2 @@
-# app/views/rates/index.json.jbuilder
 json.converted_amount @converted_amount
 json.exchange_rates @exchange_rates, :date, :rate
--- spec/requests/convert_request_spec.rb
+++ spec/requests/convert_request_spec.rb
@@ -17,29 +17,27 @@ RSpec.describe "/convert", type: :request do
     it "renders a successful response" do
       Timecop.freeze(new_time)
 
-      allow(Money).to receive(:new).and_return(Money.new(2, 'EUR'))
+      allow(Money).to receive(:new).and_return(Money.new(200, 'EUR'))
 
-      get convert_url, params: { amount: '1.0', from_currency: 'EUR', to_currency: 'USD', format: :json}
+      get convert_url, params: { cents: '100', from_currency: 'EUR', to_currency: 'USD', format: :json}
 
       expect(response).to be_successful
-      expect(response.body).to eq('{"converted_amount":0.02,"exchange_rates":[{"date":"2020-01-01","rate":1.085193082979289},{"date":"2019-12-31","rate":1.085193082979289},{"date":"2019-12-30","rate":1.085193082979289},{"date":"2019-12-29","rate":1.085193082979289},{"date":"2019-12-28","rate":1.085193082979289},{"date":"2019-12-27","rate":1.085193082979289},{"date":"2019-12-26","rate":1.085193082979289}]}')
+      expect(response.body).to eq('{"converted_amount":2.0,"exchange_rates":[{"date":"2020-01-01","rate":1.085193082979289},{"date":"2019-12-31","rate":1.085193082979289},{"date":"2019-12-30","rate":1.085193082979289},{"date":"2019-12-29","rate":1.085193082979289},{"date":"2019-12-28","rate":1.085193082979289},{"date":"2019-12-27","rate":1.085193082979289},{"date":"2019-12-26","rate":1.085193082979289}]}')
 
       Timecop.return
     end
 
     it "renders a successful response without rates" do
-      allow(Money).to receive(:new).and_return(Money.new(2, 'EUR'))
+      allow(Money).to receive(:new).and_return(Money.new(200, 'EUR'))
 
-      get convert_url, params: { amount: '1.0', from_currency: 'EUR', to_currency: 'CHF', format: :json}
+      get convert_url, params: { cents: '100', from_currency: 'EUR', to_currency: 'CHF', format: :json}
 
       expect(response).to be_successful
-      expect(response.body).to eq('{"converted_amount":0.02,"exchange_rates":[]}')
+      expect(response.body).to eq('{"converted_amount":2.0,"exchange_rates":[]}')
     end
 
     it "renders a bad request" do
-      allow(Money).to receive(:new).and_return(Money.new(2, 'EUR'))
-
-      get convert_url, params: { amount: '1.0', to_currency: 'CHF', format: :json}
+      get convert_url, params: { amount: '100', to_currency: 'CHF', format: :json}
 
       expect(response.status).to eq 400
     end
